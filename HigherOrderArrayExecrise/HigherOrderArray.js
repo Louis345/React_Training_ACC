4f339941d3d752af80599723a918d6948bb5503f
Louis345 <jt125845@gmail.com>
2018-07-09 20:22:28 -0400

    added more execrises and tests

--- HigherOrderArrayExecrise/HigherOrderArray.js
+++ HigherOrderArrayExecrise/HigherOrderArray.js
@@ -6,10 +6,51 @@ chai.use(require('sinon-chai'));
 /** for Each function should accept an array and a callback as arguments */
 forEach = (arr, callback) => {
   for (let i = 0; i < arr.length; i++) {
-    callback(arr[i]);
+    callback(arr[i], i, arr);
   }
 };
 
+/*** Write FindIndex from scartch  */
+
+findIndex = (arr, callback, index) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (callback(arr[i], i, arr)) {
+      return i;
+    }
+  }
+  return -1;
+};
+
+/*
+Write a function called onlyEvenValues which accepts an array and returns a new array with only the even values in the array passed to the function
+
+Examples:
+    onlyEvenValues([1,2,3]) // [2]
+    onlyEvenValues([5,1,2,3,10]) // [2,10]
+
+*/
+
+function onlyEvenValues(arr) {
+  const doubledNumbers = [];
+  arr.forEach((number, idx) => {
+    if (number % 2 === 0) doubledNumbers.push(number);
+  });
+  return doubledNumbers;
+}
+
+/*
+Write a function called doubleValues which accepts an array and returns a new array with all the values in the array passed to the function doubled
+
+Examples:
+    doubleValues([1,2,3]) // [2,4,6]
+    doubleValues([5,1,2,3,10]) // [10,2,4,6,20]
+
+*/
+function doubleValues(arr) {}
 module.exports = {
-  forEach
+  forEach,
+  findIndex,
+  onlyEvenValues,
+  onlyEvenValues,
+  doubleValues
 };
--- HigherOrderArrayExecrise/test/HigherOrderArray-test.js
+++ HigherOrderArrayExecrise/test/HigherOrderArray-test.js
@@ -2,15 +2,28 @@ var sinon = require('sinon');
 const chai = require('chai');
 const expect = chai.expect;
 chai.use(require('sinon-chai'));
+const assertArrays = require('chai-arrays');
+chai.use(assertArrays);
+const { forEach, findIndex, onlyEvenValues } = require('../HigherOrderArray');
 
-const { forEach } = require('../HigherOrderArray');
-
-describe('test the implmentation of forEach', () => {
+describe('testing the functionalitly of Higher Order Arrays ', () => {
   it('should  have a length of 3', () => {
     let numbersDoubled = [];
     forEach([1, 2, 3], number => {
       numbersDoubled.push(number * 2);
     });
     expect(numbersDoubled.length).to.equal(3);
+  });
+  it('should return the number 3', () => {
+    const firstDivisibleNum = findIndex([1, 2, 6], number => {
+      if (number % 2 === 0) {
+        return number;
+      }
+    });
+    expect(firstDivisibleNum).to.equal(1);
+  });
+  it.only('onlyEvenValues should return a array of even numbers', () => {
+    const evenNumbers = onlyEvenValues([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    expect(evenNumbers).to.be.equalTo([2, 4, 6, 8, 10]);
   });
 });
