6bd7a2e32905ad635cdd93cba54d5a9edbe0c10e
Louis345 <jt125845@gmail.com>
2018-07-14 13:25:23 -0400

    Updated tests

--- HigherOrderArrayExecrise/HigherOrderArray.js
+++ HigherOrderArrayExecrise/HigherOrderArray.js
@@ -21,6 +21,25 @@ findIndex = (arr, callback, index) => {
   return -1;
 };
 
+/*** Write the Map Function from  */
+const map = (arr, callback) => {
+  const newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(callback(arr[i], i, arr));
+  }
+  return newArr;
+};
+
+/*
+Write a function called doubleValues which accepts an array and returns a new array with all the values in the array passed to the function doubled
+
+Examples:
+    doubleValues([1,2,3]) // [2,4,6]
+    doubleValues([1,-2,-3]) // [2,-4,-6]
+*/
+
+function doubleValues(arr) {}
+
 /*
 Write a function called onlyEvenValues which accepts an array and returns a new array with only the even values in the array passed to the function
 
@@ -47,10 +66,58 @@ Examples:
 
 */
 function doubleValues(arr) {}
+/*
+Write a function called valTimesIndex which accepts an array and returns a new array with each value multiplied by the index it is currently at in the array.
+
+Examples:
+    valTimesIndex([1,2,3]) // [0,2,6]
+    valTimesIndex([1,-2,-3]) // [0,-2,-6]
+*/
+function valTimesIndex(arr) {}
+
+/***
+ * Create the filter array from scratch
+ *
+ */
+
+filter = (arr, callback) => {
+  const filteredArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (callback(arr[i], i, arr)) {
+      filteredArr.push(arr[i]);
+    }
+  }
+  return filteredArr;
+};
+/*
+Write a function called removeVowels which accepts a string and returns a new string with all of the vowels (both uppercased and lowercased) removed. Every character in the new string should be lowercased.
+
+Examples:
+    removeVowels('Elie') // ('l')
+    removeVowels('TIM') // ('tm')
+    removeVowels('ZZZZZZ') // ('zzzzzz')
+*/
+
+function removeVowels(str) {}
+/*
+Write a function called doubleOddNumbers which accepts an array and returns a new array with all of the odd numbers doubled (HINT - you can use map and fitler to double and then filter the odd numbers).
+
+Examples:
+    doubleOddNumbers([1,2,3,4,5]) // [2,6,10]
+    doubleOddNumbers([4,4,4,4,4]) // []
+*/
+
+function doubleOddNumbers(arr) {}
+
 module.exports = {
   forEach,
   findIndex,
   onlyEvenValues,
   onlyEvenValues,
-  doubleValues
+  doubleValues,
+  map,
+  valTimesIndex,
+  filter,
+  doubleOddNumbers,
+  removeVowels
 };
--- HigherOrderArrayExecrise/test/HigherOrderArray-test.js
+++ HigherOrderArrayExecrise/test/HigherOrderArray-test.js
@@ -4,7 +4,14 @@ const expect = chai.expect;
 chai.use(require('sinon-chai'));
 const assertArrays = require('chai-arrays');
 chai.use(assertArrays);
-const { forEach, findIndex, onlyEvenValues } = require('../HigherOrderArray');
+const {
+  forEach,
+  findIndex,
+  onlyEvenValues,
+  doubleValues,
+  map,
+  filter
+} = require('../HigherOrderArray');
 
 describe('testing the functionalitly of Higher Order Arrays ', () => {
   it('should  have a length of 3', () => {
@@ -22,8 +29,31 @@ describe('testing the functionalitly of Higher Order Arrays ', () => {
     });
     expect(firstDivisibleNum).to.equal(1);
   });
-  it.only('onlyEvenValues should return a array of even numbers', () => {
+  it('onlyEvenValues should return a array of even numbers', () => {
     const evenNumbers = onlyEvenValues([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
     expect(evenNumbers).to.be.equalTo([2, 4, 6, 8, 10]);
+  });
+  it('testing users created map function and that function should return an array of tripled values', () => {
+    const tripledValues = map([1, 2, 3], number => {
+      console.log(number);
+      return number * 3;
+    });
+    expect(tripledValues).to.be.equalTo([3, 6, 9]);
+  });
+  it('users created function should return an array with its values doubled', () => {
+    const doubledValues = doubleValues([1, 2, 3]);
+    expect(tripledValues).to.be.equalTo([2, 4, 6]);
+  });
+  it('user created function accepts an array and returns a new array with each value multiplied by the index it is currently at in the array.', () => {
+    const multipliedArr = valTimesIndex([1, 2, 3]);
+    expect(multipliedArr).to.be.equalTo([0, 2, 6]);
+  });
+  it.only('the user created filter array should return a new array with values below 21', () => {
+    const lowerThanTwentyOne = filter([10, 15, 20, 25, 30, 60], number => {
+      if (number < 21) {
+        return number;
+      }
+    });
+    expect(lowerThanTwentyOne).to.be.equalTo([10, 15, 20]);
   });
 });
