5eb2029fcf4e34e38085564ec86741d218ce8ef8
Louis345 <jt125845@gmail.com>
2018-07-21 15:48:30 -0400

    completed reduce

--- excercises/Reduce/reduce.js
+++ excercises/Reduce/reduce.js
@@ -1,2 +1,31 @@
-/** Write Reduce from scracth *
-reduce = (arr, callback) => {}
+/** Create a function that accepts an array and returns the total
+ *  Examples:
+    addItems([1,5,6]) // 12
+    addItems([1,-2,-3]) // -5
+ * 
+ *
+ */
+const addItems = arr => {};
+
+/*** Create a function that accepts an array and returns an object  with  key displaying them name of item and the value of
+ * of the item
+ **
+ */
+const generateTally = arr => {};
+
+/** create a function that accepts an array and returns a f
+ * lattened array
+ *
+ */
+
+const flattenArray = arr => {};
+
+/** convert an array of objects  to an object and the object key should be accesible by id */
+const arrayToObject = arr => {};
+
+module.exports = {
+  addItems,
+  generateTally,
+  flattenArray,
+  arrayToObject
+};
