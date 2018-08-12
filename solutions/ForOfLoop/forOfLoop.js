cca7983a26cb828a8fa3183590f914c6fb3f9f4f
Matina Patsos <mpatsos@deepbluecommunications.com>
2018-08-12 17:07:58 -0400

    Trying to restore what I accidently deleted

--- solutions/ForOfLoop/forOfLoop.js
+++ solutions/ForOfLoop/forOfLoop.js
@@ -12,7 +12,7 @@
  * contains information about a person.
  * @return {string} CSV string that you can open in Excel, Numbers or another spreadsheet program
  */
-function writeCsv(people) {
+function createCsvString(people) {
   let names = '"Name","Gender","Location","DOB"\n';
   for (let person of people) {
     let values = [];
@@ -27,5 +27,5 @@ function writeCsv(people) {
 }
 
 module.exports = {
-  writeCsv
+  createCsvString
 };
