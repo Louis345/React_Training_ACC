e8e2ea074304500793033e77536e9bd74b6f2ff4
Matina Patsos <mpatsos@deepbluecommunications.com>
2018-09-16 17:54:48 -0400

    Refactoring the tests for reduce

--- excercises/Reduce/reduce.js
+++ excercises/Reduce/reduce.js
@@ -1,56 +1,53 @@
-/** Create a function that accepts an array of numbers and returns the sum
- *
- * Examples:
- * addItems([1,5,6]) // 12
- * addItems([1,-2,-3]) // -4
+/**
+ * Given an array of numbers, return the sum
+ * @param {array} arr an array of numbers, e.g. `[1, 2, 3]`
+ * @returns sum
+ * @example
+ *  addItems([1,5,6]) // 12
+ *  addItems([1,-2,-3]) // -4
  */
-const addItems = arr => {};
+function addItems(arr) {}
 
-/** Create a function that tallies the number of each kind of fruit. It should return an object where the fruit
- * name is the key and the tally is the value.
- *
- * Example:
- *
- * var result = generateTally(['Apple', 'Orange', 'Apple', 'Blueberry', 'Grape', 'Grape'])
- * 
- * result should be:
- * {Apple: 2, Orange: 1, Blueberry: 1, Grape: 2}
- *
+/**
+ * Create a function that tallies the number of each kind of "thing" within the array
+ * @param {array} array e.g. `['Apple', 'Orange', 'Apple', 'Blueberry']`
+ * @returns {object} where the thing name is the key and the tally is the value
+ * @example
+ *   var fruits = ['Apple', 'Orange', 'Apple', 'Blueberry', 'Grape', 'Grape'];
+ *   generateTally(generateTally); // {Apple: 2, Orange: 1, Blueberry: 1, Grape: 2}
  */
+function generateTally(array) {}
 
-const generateTally = arr => {};
-
-/** Create a function that flattens an array (that is, it should "unnest" a nested array).
- *
- * Example:
- *
- * var result = flattenArray([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
- *
- * result should be: 
- * [1, 2, 3, 4, 5, 6, 7, 8, 9]
+/**
+ * Create a function that flattens an array (that is, it should "unnest" a nested array).
+ * @param {array} arr e.g. `[[1, 3], [5, 10]]`
+ * @returns {array} new, flattened array e.g. `[1, 3, 5, 10]`
  */
+function flattenArray(arr) {}
 
-const flattenArray = arr => {};
-
-/** Create a function, that when given an array of object literals, will index the object literals by a single column
- * (in this case, the ID). The function should return an object where the ID is the key and value is the the 
+/**
+ * Create a function, that when given an array of object literals, will index the object literals by a single column
+ * (in this case, the ID). The function should return an object where the ID is the key and value is the the
  * object literal.
- * 
- * Example:
+ *
+ * @param {array} arr an array of nested objects, where each object as a key called "ID"
+ * @returns {object} an object where the key is the ID and the value is the entire nested object
+ * @example
  *
  * var people = [
  *  {id: 123, name: 'Dave', age: 23},
  *  {id, 456, name: 'Rachel', age: 35}
  * ];
+ *
  * var result =  arrayToObject();
- * 
- * result should be:
+ *
+ * // result should be:
  * {
  *   123: {id: 123, name: 'Dave', age: 23},
  *   456: {id, 456, name: 'Rachel', age: 35}
  * }
  */
-const arrayToObject = arr => {};
+function arrayToObject(arr) {}
 
 module.exports = {
   addItems,
