227eb1174130ee908ca087daca2d98ba7dc69a9d
Jamal Taylor <jt125845@gmail.com>
2018-09-22 12:23:12 -0400

    Update callback.js

--- excercises/callback/callback.js
+++ excercises/callback/callback.js
@@ -1,6 +1,6 @@
 /** Complete assignment ****/
 /*** Tomorrow setup github and  setup a callback with an example of asynchronous */
-
+/*** Test
 /***
  * Do HomeWork should accept a subject, and a callback.
  * Complete this challenge by adding a two arguments 1) a subject 2) a call back
